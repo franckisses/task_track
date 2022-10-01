32917ed716ce1b1388ae201719f0bfcb86419be4
franck <franck_gxu@outlook.com>
2022-10-01 22:17:31 +0200

    task init

--- TaskReport/tasks/models.py
+++ TaskReport/tasks/models.py
@@ -5,17 +5,17 @@ from users.models import UserProfile
 
 
 class Tasks(models.Model):
-    uid = models.ForeignKey(UserProfile)
+    uid = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     country = models.CharField(max_length=32)
     operator = models.CharField(max_length=32)
     priority = models.CharField(max_length=3) # High or Low 
-    created_time = models.DateField(auto_now=True, auto_now_add=True)
+    created_time = models.DateField(auto_now_add=True)
     backend_handler = models.CharField(max_length=20, verbose_name='后段处理人')
-    frontend_handler = models.CharField(max_lenght=20, verbose_name='前端处理人')
+    frontend_handler = models.CharField(max_length=20, verbose_name='前端处理人')
     backend_finished_rate = models.FloatField(verbose_name='后段完成度')
     frontend_finished_rate = models.FloatField(verbose_name='前段完成度')
     finished_rate = models.FloatField(verbose_name='完成度')
-    deadline = models.CharField(verbose_name='最后期限')
+    deadline = models.DateField(auto_now=False,auto_now_add=False,verbose_name='最后期限')
 
     class Meta:
         db_table = 'all_tasks'
