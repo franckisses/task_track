235856918de35cd5e6c5eacb1bda23627acd4acd
franck <franck_gxu@outlook.com>
2022-10-09 22:34:39 +0200

    jquery加载文件

--- TaskReport/tasks/views.py
+++ TaskReport/tasks/views.py
@@ -1,8 +1,9 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.generic import View
 
-from utils.loging_decorator import logging_check
+from utils.loging_decorator import logging_check, get_user_by_request
 # Create your views here.
 
 
@@ -10,6 +11,18 @@ class AddTasks(View):
 
     @logging_check
     def post(self, request):
+        data = json.loads(request.body)
+        user = get_user_by_request(request)
+        if not user:
+            data = {'code': 403, 'error': 'please login again!'}
+            return JsonResponse(data)
+        project_country = data.get('project_country')
+        customer_name = data.get('customer_name')
+        task_name = data.get('task_name')
+        task_desc = data.get('task_doc')
+        files = data.get('files')
+        for key, value in data.items():
+            print(key, '-->', value)
         return JsonResponse({'code':200})
 
     def get(self,request):
